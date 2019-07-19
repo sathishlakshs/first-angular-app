f3f905784e07fbf291ced97baae260fb329af296
sathishlakshs <sathishkumar3110@gmail.com>
2019-07-19 12:46:49 +0530

    customer app logger issue resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { FormsModule } from '@angular/forms';
 import { employeeReducer } from 'src/store/reducers/employee.reducer';
 import { storeLogger } from 'ngrx-store-logger';
 import { environment } from 'src/environments/environment';
-export function logger(reducer: ActionReducer<State<any>>): any {
+export function logger(reducer: ActionReducer<any>): any {
   return storeLogger()(reducer);
 }
 export const metaReducers = environment.production ? [] : [logger];
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -2,6 +2,6 @@ import { Customer } from '../../app/model/customer.model';
 import { Employee } from 'src/app/model/employee.model';
 
 export interface AppState {
-    readonly customers: Customer[];
+    readonly customers: Customer;
     readonly employee: Employee;
 }
